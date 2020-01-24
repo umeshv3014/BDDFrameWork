f14291202b91df49f2c494101f448eaf76e8682c
umesh <umeshv3014@gmail.com>
2020-01-24 16:58:16 +0530

    bdd rest api automation famework

---
+++ src/test/java/restApiAutomation/bddFramework/AppTest.java
@@ -0,0 +1,20 @@
+package restApiAutomation.bddFramework;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
